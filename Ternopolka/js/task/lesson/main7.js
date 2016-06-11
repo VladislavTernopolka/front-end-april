69af01d02e60276982a1638de726bb0865bf0a95
Ternopolka <igorzaplavskiy@gmail.com>
2016-06-11 12:56:35 +0300

    update lessons

---
+++ Ternopolka/js/task/lesson/main7.js
@@ -0,0 +1,15 @@
+// (function() {
+//     var obj = {
+//         firstName: 'Tolya',
+//         secondN: 'Vlad',
+//         getName: function() {
+//             return this.firstName;
+//         }
+//     };
+//     var name = obj;
+//     obj = null;
+//     console.log(name.firstName);
+//     console.log(name.getName);
+//     console.log(name.getName());
+// })();
+// next task
